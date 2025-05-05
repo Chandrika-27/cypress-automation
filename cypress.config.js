c4a8de4dd3699ff1058feb349805bdeae409ca6b
Chandrika-27 <gajulachandrika.27@gmail.com>
2025-05-05 17:43:33 +0530

    updated readme and config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochaawsome',
+  reporterOptions:{
+    reporterDir:'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
+  video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config;
     },
-    specPattern: 'cypress/e2e/test/*.spec.ts'
+    specPattern: 'cypress/e2e/test/**/*.spec.{js,ts}'
   },
 });
